22ddb118625cfc00ac18b41243c0ebe617883f9f
anamariapopa <anamaria.2005p@gmail.com>
2025-10-19 23:38:59 +0300

    First 3 tasks done

---
+++ src/ArrayCalculator.java
@@ -0,0 +1,36 @@
+public class ArrayCalculator {
+
+   //1
+    public static int findMax(int[] array) {
+       int max = -1;
+       for (int i = 0; i < array.length; i++ ) {
+           if (array[i] > max) {
+               max = array[i];
+           }
+       }
+       return max;
+   }
+
+   //2
+   public static int findMin(int[] array) {
+       int min = Integer.MAX_VALUE;
+       for (int i = 0; i < array.length; i++) {
+           if (array[i] < min) {
+               min = array[i];
+           }
+       }
+       return min;
+   }
+
+   //3
+    public static int findMaxSum(int[] array) {
+        int maxSum = 0;
+        int min = findMin(array);
+        for ( int i = 0; i < array.length; i++ )
+            maxSum += array[i];
+        maxSum -= min;
+        return maxSum;
+    }
+
+
+}
